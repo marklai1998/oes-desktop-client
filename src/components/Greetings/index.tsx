c2f9011bc0d59d4a37367a0d5f4158dde8bdb2f7
_ <_@_>
2021-03-17 16:23:59 +0800

    feat: screen capture poc

--- src/components/Greetings/index.tsx
+++ src/components/Greetings/index.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Container, Image, Text } from './styles';
+import { desktopCapturer } from 'electron';
 
 const Greetings: React.FC = () => {
+  const handleStream = (stream) => {
+    const video = document.querySelector('video');
+    video.srcObject = stream;
+    video.onloadedmetadata = (e) => video.play();
+  };
+
+  const handleError = (e) => {
+    console.log(e);
+  };
+
+  useEffect(() => {
+    desktopCapturer
+      .getSources({ types: ['screen'] })
+      .then(async (sources) => {
+        console.log(sources);
+        for (const source of sources) {
+          try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+              audio: false,
+              video: {
+                mandatory: {
+                  chromeMediaSource: 'desktop',
+                  chromeMediaSourceId: source.id,
+                  minWidth: 1280,
+                  maxWidth: 1280,
+                  minHeight: 720,
+                  maxHeight: 720,
+                },
+              },
+            });
+            handleStream(stream);
+          } catch (e) {
+            handleError(e);
+          }
+          return;
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+
   return (
     <Container>
       <Image
@@ -10,8 +53,9 @@ const Greetings: React.FC = () => {
         alt="ReactJS logo"
       />
       <Text>
-        An Electron boilerplate including TypeScript, React, Jest and ESLint.
+        A lectron boilerplate including TypeScript, React, Jest and ESLint.
       </Text>
+      <video />
     </Container>
   );
 };
